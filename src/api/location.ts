8ab41c66cca8159ebceb4f78dcb9a70e52d87b7d
E233-3639 <ryoogisoe233@gmail.com>
2024-07-19 13:34:43 +0900

    add: flask-react-exampleの実装を移植(不具合あり)
    flask-react-exampleの例を移植しましたが、列車位置を仮想的に演算しているAPIから上手く値を受け取れていないのか、路線図が表示されません。

---
+++ src/api/location.ts
@@ -0,0 +1,24 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+    let trainState: TrainState = { "t0": { mileage: 0.0 } };
+    trainState = testTrainLocation(trainState);
+    return c.json({ trainState });
+})
+
+type TrainState = {
+    [key: string]: { mileage: number }
+};
+
+function testTrainLocation(trainState: TrainState): TrainState {
+    for (let key in trainState) {
+        if (trainState.hasOwnProperty(key)) {
+            trainState[key].mileage = (trainState[key].mileage + 0.5) % 100;
+        }
+    }
+    return trainState;
+};
+
+export { app, TrainState };
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { app as indexPage } from './pages/index';
+import { app as trainMapPage } from './pages/TrainMap';
+import { app as locationApi } from './api/location';
 
 const app = new Hono();
 
@@ -9,6 +11,8 @@ app.use(logger());
 
 // pages
 app.route('/', indexPage);
+app.route('/trainMap', trainMapPage);
+app.route('/api/location', locationApi);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
