b00dc01097ff41958bb434dca5d0d09ee5ab96a7
E233-3639 <ryoogisoe233@gmail.com>
2024-07-19 13:27:41 +0900

    refactor: src/app.tsの軽微な修正

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 import { logger } from 'hono/logger';
-import { app as indexRouter } from './pages/index'
+import { app as indexPage } from './pages/index';
 
 const app = new Hono();
 
 app.use(logger());
 
 // pages
-app.route('/', indexRouter);
+app.route('/', indexPage);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
